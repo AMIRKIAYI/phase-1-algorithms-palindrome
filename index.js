4e57c5d603d0fac7e8fc0bb021a406217d938fb9
AMIRKIAYI <amirkiayi516@gmail.com>
2024-06-01 12:31:30 +0300

    index.js file update

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+//declare a funtion
+function isPalindrome(string){
+  //create a container to store reversed name
+  let reversedName = string.split("").reverse().join("");
+  //write the condition
+  if(string == reversedName){
+      return true
+  }else{
+      return false
+  }
 }
+//call the function to display the result in the  console
+console.log(isPalindrome("amir"))
+
 
 /* 
-  Add your pseudocode here
+  PSEUDOCODE
+  1. Declare a function 
+  2. Use if else condition
+     if string is palindrome, return true else return false
+  3. call the function   
 */
 
 /*
